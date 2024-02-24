487470562b83376c823a0dd6d9e9ee4b388fb205
Silvana Alves <silvanarodriguesalvess@gmail.com>
2024-02-24 13:07:35 -0300

    fixing the movements names

---
+++ WWDC24.swiftpm/Views/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Silvana Rodrigues Alves on 23/02/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
